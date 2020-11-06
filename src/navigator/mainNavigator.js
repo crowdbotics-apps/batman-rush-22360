a43c97f75403c910735a0b50a10d4ee6088ab0af
Crowdbotics <team@crowdbotics.com>
2020-11-06 21:24:37 +0000

    add package blueprint-react-native-sign-in-4-screen

--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -5,6 +5,7 @@ import {createDrawerNavigator} from 'react-navigation-drawer';
 import SplashScreen from "../features/SplashScreen";
 import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
+import SignIn44168584Navigator from '../features/SignIn44168584/navigator';
 import Tutorial4168583Navigator from '../features/Tutorial4168583/navigator';
 import Settings5168582Navigator from '../features/Settings5168582/navigator';
 import BlankScreen0168580Navigator from '../features/BlankScreen0168580/navigator';
@@ -16,6 +17,7 @@ import BlankScreen0168580Navigator from '../features/BlankScreen0168580/navigato
 const AppNavigator = {
 
     //@BlueprintNavigationInsertion
+SignIn44168584: { screen: SignIn44168584Navigator },
 Tutorial4168583: { screen: Tutorial4168583Navigator },
 Settings5168582: { screen: Settings5168582Navigator },
 BlankScreen0168580: { screen: BlankScreen0168580Navigator },
